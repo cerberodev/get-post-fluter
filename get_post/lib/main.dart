a6fe24247f5fd5d74b7d58fa293823104613416e
Pierre Guillen <hi@cerbero.dev>
2019-11-13 14:41:29 -0500

    Update 0.2

--- get_post/lib/main.dart
+++ get_post/lib/main.dart
@@ -17,12 +17,54 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  Map data;
+  List userData;
+
+  Future getData() async {
+    http.Response response = await http.get("https://reqres.in/api/users?page=2");
+    data = json.decode(response.body);
+    setState(() {
+      userData = data["data"];
+    });
+  }
+
+
   @override
+  void initState() { 
+    super.initState();
+    getData();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Cerbero - Dev"),
+        backgroundColor: Colors.deepPurpleAccent,
+        centerTitle: true,
+      ),
+      body: ListView.builder(
+        itemCount: userData == null ? 0 : userData.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+            child: Row(
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundImage: NetworkImage(userData[index]["avatar"]),
+                ),
+                Divider(height: 5),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text("${userData[index]["first_name"]} ${userData[index]["last_name"]}"),
+                )
+              ],
+            ),  
+          );
+        },
       ),
     );
   }
+
+
+  
 }
